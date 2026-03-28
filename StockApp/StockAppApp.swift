0557ee41bc732af4b8314f933021bca02f594283
Leandro Nascimento <fleandro.nascimento@gmail.com>
2026-03-28 10:49:32 -0300

    Initial Commit

---
+++ StockApp/StockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockAppApp.swift
+//  StockApp
+//
+//  Created by Leandro Nascimento on 28/03/26.
+//
+
+import SwiftUI
+
+@main
+struct StockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
